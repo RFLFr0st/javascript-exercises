07d9d2ed212af99fc5a5f1abdabfae3982d50377
Rafael Andrade <109153310+RafaelAndrade25@users.noreply.github.com>
2023-09-05 20:28:08 -0300

    Add files via upload

---
+++ api.js
@@ -0,0 +1,30 @@
+/*Função é assincrona, pois ela vai fazer uma requisição para a API 
+e não vai esperar a resposta para continuar a execução do código
+isso é uma promisse que vai ser resolvida quando a resposta chegar */
+
+async function buscaCep(){
+    //pega o valor do input
+    let cep = document.getElementById('cep').value
+
+    //faz a requisição para a API
+    //await é aguardar pela resposta da requisição (promise)
+    let resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+
+    //await é uma forma de aguardar a resposta da api pois existe uma promessa de que haverá um retorno(promise)
+    //fetch é uma função que faz a requisição para a API
+
+    //transforma a resposta em um objeto
+    let dados = await resposta.json()
+
+    //pega o elemento que vai ser alterado
+    document.getElementById('logradouro').value = dados.logradouro
+    document.getElementById('complemento').value = dados.complemento
+    document.getElementById('bairro').value = dados.bairro
+    document.getElementById('localidade').value = dados.localidade
+    document.getElementById('estado').value = dados.uf
+    document.getElementById('ibge').value = dados.ibge
+    document.getElementById('gia').value = dados.gia
+    document.getElementById('ddd').value = dados.ddd
+    document.getElementById('siafi').value = dados.siafi
+    
+}
